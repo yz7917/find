7a75dc8e3341b795092a142dcc0b5ee46f98e8a2
YIPENG ZHOU <yipengzhou@yipengdemacbook-air.local>
2018-03-11 01:40:59 +0000

    git commit -m

--- find/main.cpp
+++ find/main.cpp
@@ -1,15 +1,27 @@
-//
-//  main.cpp
-//  find
-//
-//  Created by YIPENG ZHOU on 2018/3/10.
-//  Copyright © 2018年 joe. All rights reserved.
-//
-
+#include <algorithm> // for std::find
+#include <iterator> // for std::begin, std::end
+using namespace std;
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main ()
+{
+    int a[] = {3, 6, 8, 33};
+    int x[] = {3, 4};
+    int* adj = new int[66];
+    for(int i=0; i<2; i++){
+        bool exists = find(begin(a), end(a), x[i]) != end(a);
+        adj[i] = exists;
+        std::cout << std::boolalpha << exists << '\n';
+        cout <<adj[i]<<endl;
+    }
+    cout << endl;
+    int k=0;
+    for(int i=0; i<4; i++){
+        if(adj[i]==1){
+            int* t = new int[k];
+            t[k] = a[i];
+            cout << t[k]<<endl;
+            k++;
+        }
+    }
+    cout << k << endl;
 }
